3d17ef6e9ccccdce09b7def01a8c8ba3c1a51ac1
aponkhan21654 <146560021+aponkhan21654@users.noreply.github.com>
2026-07-12 01:33:52 +0600

    Remove debug signing configuration
    Removed debug signing configuration from build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,7 @@ android {
       isShrinkResources = true
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
-    }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+    } }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
